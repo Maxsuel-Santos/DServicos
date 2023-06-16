4c8ea9c7b93e3785831335582ca80545a578e37e
Maxsuel Santos <72948249+Maxsuel-Santos@users.noreply.github.com>
2023-06-15 21:19:22 -0300

    Adição dos arquivos

---
+++ includes/dados.php
@@ -0,0 +1,28 @@
+<?php
+
+$cardsInfo = [
+    [
+        "img" => "assets/img/eletricista.png",
+        "servico" => "Serviço de Eletricista"
+    ],
+    [
+        "img" => "assets/img/mudancas.png",
+        "servico" => "Mudanças e Carretos"
+    ],
+    [
+        "img" => "assets/img/pedreiro.png",
+        "servico" => "Serviço de Pedreiro"
+    ],
+    [
+        "img" => "assets/img/montador.png",
+        "servico" => "Montagem de Móveis"
+    ]
+    ,[
+        "img" => "assets/img/diarista.png",
+        "servico" => "Serviços de Diarista"
+    ],
+    [
+        "img" => "assets/img/pintura.png",
+        "servico" => "Serviço de Pintura"
+    ]
+];
